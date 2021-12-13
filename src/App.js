7502446afe6002fabcc185456bdddc94214edb52
GlebKolyano <glebkolyano38@gmail.com>
2021-12-13 18:39:33 +0300

    Create a basis of a chat app using ChateEngine.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="5a87c640-9704-4722-ac84-853fdc7f5e87"
+      userName="Gleb"
+      userSecret="neformal123" 
+    />
   );
 }
 
