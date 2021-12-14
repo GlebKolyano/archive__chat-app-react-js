49de0d6d9017fa4c62d2720b78d0cedb6df7fb31
GlebKolyano <glebkolyano38@gmail.com>
2021-12-14 12:30:48 +0300

    Implement LoginForm component.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  if(!localStorage.getItem("username")) return <LoginForm />
   return (
     <ChatEngine
       height="100vh"
       projectID="5a87c640-9704-4722-ac84-853fdc7f5e87"
-      userName="Gleb"
-      userSecret="neformal123" 
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/> }
     />
   );
